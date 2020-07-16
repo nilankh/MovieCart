6f79f42ce9be6bd5cd08021ed953e99a9fe87fc0
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-07-16 18:19:08 +0530

    adding state in cartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,46 @@
 import React, { Component } from "react";
 
 export class CartItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      price: 999,
+      title: "Phone",
+      qty: 1,
+      img: "",
+    };
+  }
+
   render() {
+      const { price, title, qty } = this.state
     return (
       <div className="cart-item">
         <div className="left-block">
-            <img style={styles.image}/>
+          <img style={styles.image} />
         </div>
         <div className="right-block">
-            <div style={{ fontSize: 25 }}>Phone</div>
-            <div style={{ color: "#777" }}>Rs 999</div>
-            <div style={{ color: "#777" }}>Qty: 1</div>
-            <div className="cart-item-actions">
-                {/* buttons */}
-            </div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {price}</div>
+        <div style={{ color: "#777" }}>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            {/* buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1828/1828906.svg"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            />
+          </div>
         </div>
       </div>
     );
@@ -21,11 +48,11 @@ export class CartItem extends Component {
 }
 
 const styles = {
-    image: {
-      height: 110,
-      width: 110,
-      borderRadius: 4,
-      background: "#ccc",
-    },
-  };
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
 export default CartItem;
